28b199da841a4a77fb54f0f2f68b576065c0d5ee
Phoenix <lanyusea@gmail.com>
2015-09-24 23:13:03 +0800

    publish all broadcast data received

--- Onboard_SDK_Sample/DJI_Onboard_API_ROS_Sample/include/djiPublisher.h
+++ Onboard_SDK_Sample/DJI_Onboard_API_ROS_Sample/include/djiPublisher.h
@@ -4,9 +4,9 @@
 #include "SDK.h"
 namespace publishers
 {
-	extern ros::Publisher battery_pub, nav_ctrl_status_pub,
-			 flight_status_pub, activation_status_pub, test_fre_pub, acc_pub;
-	extern ros::Publisher gps_pub, att_quad_pub,
+	extern ros::Publisher battery_pub, ctrl_info_pub,
+			 is_flying_pub, activation_status_pub, test_fre_pub, acc_pub;
+	extern ros::Publisher gps_pub, att_quad_pub, compass_pub,
 			 vel_pub, local_pos_pub;
 	extern ros::Publisher rc_channels_pub,odem_publisher;
 	int init_publishers(ros::NodeHandle & nh);
