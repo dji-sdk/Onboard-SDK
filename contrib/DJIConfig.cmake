d69aa45aa4038cf995dd3a1c8bc4ceb9862fc61e
bastien <bastien.auneau@while-true.fr>
2017-04-04 14:05:41 +0000

    STREQUAL not recognized in cmake 3.7.2. Searching in all -release files for version

--- contrib/DJIConfig.cmake
+++ contrib/DJIConfig.cmake
@@ -9,7 +9,7 @@ endif()
 
 #Detect platform - from https://gist.github.com/CoolerVoid/1781717
 EXECUTE_PROCESS(
-  COMMAND cat /etc/lsb-release
+  COMMAND `cat /etc/*-release`
   COMMAND grep DISTRIB_RELEASE
   COMMAND awk -F= "{ print $2 }"
   COMMAND tr "\n" " "
@@ -17,11 +17,11 @@ EXECUTE_PROCESS(
   OUTPUT_VARIABLE LSB_VER
   )
 
-if( ${LSB_VER} STREQUAL "16.04")
+if( ${LSB_VER} MATCHES "16.04")
   set(DISTRO_VERSION 1604)
-elseif(${LSB_VER} STREQUAL "14.04")
+elseif(${LSB_VER} MATCHES "14.04")
   set(DISTRO_VERSION 1404)
-elseif(${LSB_VER} STREQUAL "18")
+elseif(${LSB_VER} MATCHES "18")
   set(DISTRO_VERSION 1604)
 else()
   set(DISTRO_VERSION UNKNOWN)
