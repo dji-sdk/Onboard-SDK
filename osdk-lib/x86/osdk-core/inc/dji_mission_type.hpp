30aa7224a7f4c9594e9ea1e3dfedf26c7d735ffe
sandy.yu <yusongzhou0612@163.com>
2020-04-27 11:42:00 +0800

    fix: fix the waypoint v2 sample pause failed
    OSDK-2428

--- osdk-lib/x86/osdk-core/inc/dji_mission_type.hpp
+++ osdk-lib/x86/osdk-core/inc/dji_mission_type.hpp
@@ -1109,8 +1109,8 @@ typedef struct WayPointV2InitSettings{
   uint16_t missTotalLen;
 
   /**
-   *  Mission execution can be repeated more than once. A value of 0 means the mission
-   *  only executes once, and does not repeat. A value of 1 means the mission will
+   *  Mission execution can be repeated more than once. A value of 1 means the mission
+   *  only executes once, and does not repeat. A value of 2 means the mission will
    *  execute a total of two times.
    */
   uint8_t repeatTimes;
