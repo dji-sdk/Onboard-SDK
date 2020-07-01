85c08690e8a903839d4dfe6e2a7a88652d8f2897
DJI-Jerry <dev.osdk.jerry@dji.com>
2020-07-01 11:28:00 +0800

    new: add more mop transmission samples.
    OSDK-2554

--- sample/platform/linux/mop/mop_sample_simple_protocol.hpp
+++ sample/platform/linux/mop/mop_sample_simple_protocol.hpp
@@ -5,6 +5,51 @@
 #ifndef ONBOARDSDK_INTERNAL_ONBOARD_SDK_SAMPLE_PLATFORM_LINUX_MOP_MOP_SAMPLE_SIMPLE_PROTOCOL_HPP_
 #define ONBOARDSDK_INTERNAL_ONBOARD_SDK_SAMPLE_PLATFORM_LINUX_MOP_MOP_SAMPLE_SIMPLE_PROTOCOL_HPP_
 
+#define UTIL_OFFSETOF(type, member)                ((size_t) & ((type *)0 )-> member)
+
+typedef enum {
+    FILE_TRANSFOR_CMD_REQUEST = 0x50,
+    FILE_TRANSFOR_CMD_ACK = 0x51,
+    FILE_TRANSFOR_CMD_RESULT = 0x52,
+    FILE_TRANSFOR_CMD_FILE_INFO = 0x60,
+    FILE_TRANSFOR_CMD_FILE_DOWNLOAD_REQ = 0x61,
+    FILE_TRANSFOR_CMD_FILE_DATA = 0x62,
+    FILE_TRANSFOR_CMD_STOP = 0x62,
+} T_MopChannel_FileTransforCmd;
+
+typedef enum {
+    FILE_TRANSFOR_SUBCMD_REQUEST_UPLOAD = 0x00,
+    FILE_TRANSFOR_SUBCMD_REQUEST_DOWNLOAD = 0x01,
+} T_MopChannel_FileTransforRequestSubCmd;
+
+typedef enum {
+    FILE_TRANSFOR_SUBCMD_ACK_OK = 0x00,
+    FILE_TRANSFOR_SUBCMD_ACK_REJECTED = 0x01,
+} T_MopChannel_FileTransforAckSubCmd;
+
+typedef enum {
+    FILE_TRANSFOR_SUBCMD_RESULT_OK = 0x00,
+    FILE_TRANSFOR_SUBCMD_RESULT_FAILED = 0x01,
+} T_MopChannel_FileTransforResultSubCmd;
+
+typedef enum {
+    FILE_TRANSFOR_SUBCMD_FILE_INFO_DEFAULT = 0xFF,
+} T_MopChannel_FileTransforFileInfoSubCmd;
+
+typedef enum {
+    FILE_TRANSFOR_SUBCMD_DOWNLOAD_REQUEST = 0xFF,
+} T_MopChannel_FileTransforFileDownloadRequestSubCmd;
+
+typedef enum {
+    FILE_TRANSFOR_SUBCMD_FILE_DATA_NORMAL = 0x00,
+    FILE_TRANSFOR_SUBCMD_FILE_DATA_END = 0x01,
+} T_MopChannel_FileTransforFileDataSubCmd;
+
+typedef enum {
+    FILE_TRANSFOR_SUBCMD_STOP_UPLOAD = 0x00,
+    FILE_TRANSFOR_SUBCMD_STOP_DOWNLOAD = 0x01,
+} T_MopChannel_FileTransforStopSubCmd;
+
 typedef enum CmdEnum : uint8_t {
   CMD_REQUEST = 0x50,
   CMD_ACK = 0x51,
